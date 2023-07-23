2c6ec45a233fefb539c884f2421e2a7834641d85
Manish Sadhu <sadhumanishkumar@gmail.com>
2023-07-23 14:57:24 +0530

    Feat: Happy Path

---
+++ server/index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+// const allowedOrigins = ['http://localhost:3000'];
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from the Express server!');
+});
+
+// API route to validate credit card number
+app.post('/validate-credit-card', (req, res) => {
+  let { cardNumber } = req.body;
+
+   // Check if the credit card number is present and contains only digits
+   console.log('Received cardNumber:', cardNumber);
+   if (typeof cardNumber === 'number') {
+    cardNumber = cardNumber.toString()
+  }
+
+   // Check if the length of the credit card number is greater than or equal to 16
+   console.log('Card number length:', cardNumber.length);
+   if (cardNumber.length < 15) {
+     console.log('Invalid credit card number length:', cardNumber);
+     return res.status(400).json({ error: 'Invalid credit card number length' });
+   }
+
+
+  // Check if the credit card number is present and contains only digits
+  if (!cardNumber || !/^\d+$/.test(cardNumber)) {
+    console.log('Invalid credit card number format:', cardNumber);
+    return res.status(400).json({ error: 'Invalid credit card number format' });
+  }
+  
+  // Validate the credit card number using the Luhn algorithm
+  if (!luhnAlgorithmValidation(cardNumber)) {
+    console.log('Invalid credit card number:', cardNumber);
+    return res.status(400).json({ error: 'Invalid credit card number' });
+  }
+
+  // If all validations pass, respond with a success message
+   console.log('Credit card number is valid:', cardNumber);
+  return res.json({ message: 'Credit card number is valid' });
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+
+// Luhn algorithm implementation
+function luhnAlgorithmValidation(cardNumber) {
+
+  // Remove any non-digit characters from the card number
+  const sanitizedCardNumber = cardNumber.replace(/\D/g, '');
+
+  let sum = 0;
+  let isEven = false;
+
+  // Starting from the rightmost digit, double every second digit
+  for (let i = sanitizedCardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(sanitizedCardNumber.charAt(i), 10);
+
+    if (isEven) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    isEven = !isEven;
+  }
+
+  // If the sum is divisible by 10, the card number is valid
+  return sum % 10 === 0;
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Form, Button, Modal } from "rsuite";
+import "./App.css";
+import "rsuite/dist/rsuite.min.css";
+import CheckOutlineIcon from "@rsuite/icons/CheckOutline";
+import CloseOutlineIcon from "@rsuite/icons/CloseOutline";
 
 function App() {
+  const [creditCardNumber, setCreditCardNumber] = useState("");
+  const [formattedCardNumber, setFormattedCardNumber] = useState("");
+  const [isValidCard, setIsValidCard] = useState(false);
+  const [modalErrorMessage, setModalMessage] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  // Function to validate the credit card number
+  async function validateCreditCard(cardNumber) {
+    try {
+      const response = await fetch(
+        "http://localhost:8080/validate-credit-card",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ cardNumber: cardNumber }),
+        }
+      );
+      setCreditCardNumber("");
+
+      if (response.ok) {
+        setShowModal(true);
+        setIsValidCard(true);
+      } else {
+        setShowModal(true);
+        setIsValidCard(false);
+      }
+
+      setShowModal(true);
+    } catch (error) {
+      setModalMessage("Error validating credit card.");
+      setShowModal(true);
+    }
+  }
+
+  const handleCardNumberChange = (value) => {
+    // Remove any non-digit characters from the input
+    const inputText = value.replace(/\D/g, "");
+
+    // Add spaces after every four digits
+    const formattedNumber = inputText.replace(/(\d{4})(?=\d)/g, "$1 ");
+
+    setCreditCardNumber(formattedNumber);
+    setFormattedCardNumber(formattedNumber);
+  };
+
+  // Function to handle form submission
+  async function handleSubmit() {
+    await validateCreditCard(creditCardNumber.replace(/\s/g, ""));
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div style={{ maxWidth: 400, padding: 20 }}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.ControlLabel style={{ fontWeight: "bold" }}>
+              Enter Credit Card Number:
+            </Form.ControlLabel>
+            <Form.Control
+              className="credit-card-input"
+              name="cardNumber"
+              type="text"
+              value={creditCardNumber}
+              onChange={handleCardNumberChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button
+              type="submit"
+              appearance="primary"
+              disabled={creditCardNumber.length === 0}
+            >
+              Validate
+            </Button>
+          </Form.Group>
+        </Form>
+
+        <Modal
+          open={showModal}
+          onClose={handleCloseModal}
+          onHide={handleCloseModal}
         >
-          Learn React
-        </a>
-      </header>
+          <Modal.Header>
+            <Modal.Title>Validation Result</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {modalErrorMessage ? (
+              <p>{modalErrorMessage}</p>
+            ) : (
+              <p>
+                Cerdit Card : <b>{formattedCardNumber}</b> is  {" "}
+                <b>
+                  {isValidCard ? (
+                    <>
+                       Valid <CheckOutlineIcon color="#6cc5b4" />
+                    </>
+                  ) : (
+                    <>
+                      Invalid <CloseOutlineIcon color="#FF0000" />
+                    </>
+                  )}{" "}
+                </b>
+              </p>
+            )}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={handleCloseModal} appearance="primary">
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </div>
   );
 }
